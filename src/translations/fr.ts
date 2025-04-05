83fc7f8ba3611740e7cd8e7fe40f49aca757293b
Stéphanie Desby <stephanie@time2genius.com>
2025-04-05 18:29:12 +0200

    Change French wording to make it pop

--- src/translations/fr.ts
+++ src/translations/fr.ts
@@ -1,8 +1,8 @@
 export const fr = {
-  title: "😐 Pas très motivé ?",
-  subtitle: "Appuie sur le Bouton Magique et reçois une mini mission pour pimenter ta journée. 🌶️",
-  mainButton: "Libère-moi de l'ennui !",
-  anotherButton: "Encore une !",
+  title: "😐 Envie de secouer la routine ?",
+  subtitle: "Tape sur le Bouton Magique et pour changer ta journée !",
+  mainButton: "Sauve-moi de l’ennui !",
+  anotherButton: "Une autre dose !",
   errors: {
     default: "Une erreur inattendue s'est produite",
     failedToSend: "Échec de l'envoi du sentiment"
